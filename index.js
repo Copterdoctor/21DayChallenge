784f6c2b508eefa7ce264f4f0d436af1271e2fab
CopterDoctor <Jordananders@me.com>
2018-10-20 08:54:36 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,157 @@
+const GRID = [
+  ["", "", "", "^", "", "", "", "", "", ""],
+  ["", "", "v", "", "~", "", "", "", "", ""],
+  ["", "v", "", "", "^", "^", "", "", "", ""],
+  ["", "", "", "", "^", "^", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "v", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", "", ""],
+  ["", "^", "~", "~", "", "", "", "^", "", ""],
+  ["", "^", "", "~", "~", "", "", "", "", ""],
+  ["", "^", "", "", "~", "~", "", "", "", ""],
+];
+
+function countRows() {
+  return GRID.length;
+}
+
+function countColumns() {
+  return GRID[0].length;
+}
+
+function gridSize() {
+  return (countColumns() + " x " + countRows());
+}
+
+function totalCells() {
+  return (countColumns() * countRows());
+}
+
+function convertColumn(location) {
+  var char = location[0].toLowerCase();
+  return (char.charCodeAt(0) - 97);
+}
+
+function isRock(location) {
+  return (lightCell(location) == "^" ? true : false);
+}
+
+function isCurrent(location) {
+  return (lightCell(location) == "~" ? true : false);
+}
+
+function isShip(location) {
+  return (lightCell(location) == "v" ? true : false);
+}
+
+function lightRow(rowNum) {
+  return (GRID[rowNum - 1]);
+}
+
+function lightColumn(column) {
+  var columnContents = [];
+  var char = column.toLowerCase();
+  var columnNum = char.charCodeAt(0) - 97;
+  GRID.map(e => {
+    columnContents.push(e[columnNum]);
+  });
+  return columnContents;
+}
+
+function lightCell(location) {
+  var row = parseInt(location.slice(1));
+  var columnIndex = convertColumn(location);
+  var numOfColumns = countColumns();
+  var numOfRows = countRows();
+
+  if (numOfRows < row || numOfColumns < (columnIndex + 1)) {
+    return false;
+  } else {
+    return GRID[row - 1][columnIndex];
+  }
+}
+
+// GRID[0] is required to determin how many columns the GRID has
+function allRocks() {
+  let rocks = [];
+  for (let i = 0; i < GRID.length; i++) {
+    for (let j = 0; j < GRID[0].length; j++) {
+      let e = GRID[i][j];
+      if (e === "^") {
+        let location = `${String.fromCharCode(j + 65)}${i + 1}`;
+        rocks.push(location);
+      }
+    }
+  }
+  return rocks;
+}
+
+// GRID[0] is required to determin how many columns the GRID has
+function allCurrents() {
+  let currents = [];
+  for (let i = 0; i < GRID.length; i++) {
+    for (let j = 0; j < GRID[0].length; j++) {
+      let e = GRID[i][j];
+      if (e === "~") {
+        let location = `${String.fromCharCode(j + 65)}${i + 1}`;
+        currents.push(location);
+      }
+    }
+  }
+  return currents;
+}
+
+function allShips() {
+  let ships = [];
+  for (let i = 0; i < GRID.length; i++) {
+    for (let j = 0; j < GRID[0].length; j++) {
+      let e = GRID[i][j];
+      if (e === "v") {
+        let location = `${String.fromCharCode(j + 65)}${i + 1}`;
+        ships.push(location);
+      }
+    }
+  }
+  return ships;
+}
+
+function firstRock() {
+  var rocks = allRocks();
+  return rocks[0];
+}
+
+function firstCurrent() {
+  var currents = allCurrents();
+  return currents[0];
+}
+
+function shipReport() {
+  let result = [];
+  const ships = allShips().sort();
+  result.push(ships.shift());
+  result.push(ships.pop());
+  return result;
+}
+
+shipReport();
+
+module.exports = {
+  countRows,
+  countColumns,
+  gridSize,
+  totalCells,
+  convertColumn,
+  lightCell,
+  isRock,
+  isCurrent,
+  isShip,
+  lightRow,
+  lightColumn,
+  lightCell,
+  allRocks,
+  allCurrents,
+  allShips,
+  firstRock,
+  firstCurrent,
+  shipReport,
+}
---
+++ test/test.js
@@ -0,0 +1,133 @@
+const assert = require('chai').assert
+const expect = require('chai').expect
+const app = require('../index');
+
+describe('21dayCodingChallenge', () => {
+  describe('countRows', () => {
+    it('Should return the number of rows', () => {
+      assert.equal(app.countRows(), 10);
+    });
+  });
+
+  describe('countColumns', () => {
+    it('Should return the number of columns', () => {
+      assert.equal(app.countColumns(), 10);
+    });
+  });
+
+  describe('gridSize', () => {
+    it('Should return the size of the grid as a string', () => {
+      assert.typeOf(app.gridSize(), 'string');
+    });
+    it('Should return 10 x 10', () => {
+      assert.deepEqual(app.gridSize(), '10 x 10');
+    });
+  });
+
+  describe('totalCells', () => {
+    it('Should return total count of cells in grid', () => {
+      assert.equal(app.totalCells(), 100);
+    });
+  });
+
+  describe('convertColumn', () => {
+    it('Should convert column to index of array', () => {
+      assert.equal(app.convertColumn('A1'), 0);
+    });
+  });
+
+  describe('isRock', () => {
+    it('Should return true if cell contents are ^', () => {
+      assert.equal(app.isRock('D1'), true)
+    })
+    it('Should return false if cell contents are not ^', () => {
+      assert.equal(app.isRock('E1'), false)
+    })
+  })
+
+  describe('isCurrent', () => {
+    it('Should return true if cell contents are ~', () => {
+      assert.equal(app.isCurrent('E2'), true)
+    })
+    it('Should return false if cell contents are not ~', () => {
+      assert.equal(app.isCurrent('E3'), false)
+    })
+  })
+
+  describe('isShip', () => {
+    it('Should return true if cell contents are v', () => {
+      assert.equal(app.isShip('C2'), true)
+    })
+    it('Should return false if cell contents are not v', () => {
+      assert.equal(app.isShip('C4'), false)
+    })
+  })
+
+  describe('lightRow', () => {
+    it('Should return contents of row as Array', () => {
+      assert.isArray(app.lightRow(1))
+    })
+    it('Should return contents of row', () => {
+      expect(app.lightRow(1)).to.deep.equal(["", "", "", "^", "", "", "", "", "", ""])
+    })
+  })
+
+  describe('lightColumn', () => {
+    it('Should return contents of column as Array', () => {
+      assert.isArray(app.lightColumn('A'), 'Column is not an array')
+    })
+    it('Should return contents of column', () => {
+      expect(app.lightColumn('B')).to.deep.equal(["", "", "v", "", "", "", "", "^", "^", "^"], "Did not contain expect contents")
+    })
+  })
+
+  describe('lightCell', () => {
+    it('Should return contents of selected cell D1', () => {
+      assert.equal(app.lightCell('D1'), '^')
+    })
+    it('Should return false if column doesnt exist', () => {
+      assert.equal(app.lightCell('K1'), false)
+    })
+    it('Should return false if row doesnt exist', () => {
+      assert.equal(app.lightCell('J11'), false)
+    })
+  })
+
+  describe('allRocks', () => {
+    it('Should return cell locations with rocks ^', () => {
+      assert.deepEqual(app.allRocks(), ['D1', 'E3', 'F3', 'E4', 'F4', 'B8', 'H8', 'B9', 'B10'])
+    })
+  })
+
+  describe('allCurrents', () => {
+    it('Should return call locations with strong currents ~', () => {
+      assert.deepEqual(app.allCurrents(), ['E2', 'C8', 'D8', 'D9', 'E9', 'E10', 'F10'])
+    })
+  })
+
+  describe('allShips', () => {
+    it('Should return call locations with ships v', () => {
+      assert.deepEqual(app.allShips(), ['C2', 'B3', 'I5'])
+    })
+  })
+
+  describe('firstRock', () => {
+    it('Should return the coordinates of the first rock', () => {
+      assert.equal(app.firstRock(), 'D1')
+    })
+  })
+
+  describe('firstCurrent', () => {
+    it('Should return the coordinates of the first current', () => {
+      assert.equal(app.firstCurrent(), 'E2')
+    })
+  })
+
+  describe('shipReport', () => {
+    it('Should return an array of the coordinates of the ship furthest to the west (left) and east (right) of your GRID', () => {
+      assert.deepEqual(app.shipReport(), ['B3', 'I5'])
+    })
+  })
+
+
+})
